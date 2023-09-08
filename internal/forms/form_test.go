85f1b0f1b519e1d7a5861d265a9d35bb43c9e277
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-09-08 16:55:23 -0300

    testing required field in a form

---
+++ internal/forms/form_test.go
@@ -0,0 +1,39 @@
+package forms
+
+import (
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields are missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "b")
+	postedData.Add("c", "c")
+
+	r = httptest.NewRequest("POST", "/whatever", nil)
+	r.PostForm = postedData
+	form = New(r.PostForm)
+	if !form.Valid() {
+		t.Error("show does not have required fields when it does")
+	}
+}

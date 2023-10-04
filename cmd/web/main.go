1f4401ad0971a6b0f9aca91a270c52bc81274413
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-10-04 18:57:55 -0300

    building send email from dummy mail server utilize go routine and channel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,6 +31,20 @@ func main() {
 	}
 	defer db.SQL.Close()
 
+	defer close(app.MailChan)
+
+	fmt.Println("Starting mail listener...")
+	listenForMail()
+
+	// msg := models.MailData{
+	// 	To:      "john@do.ca",
+	// 	From:    "me@here.com",
+	// 	Subject: "Some subject",
+	// 	Content: "",
+	// }
+
+	// app.MailChan <- msg
+
 	fmt.Printf("starting application on port %s \n", PORT)
 
 	srv := &http.Server{
@@ -48,6 +62,9 @@ func run() (*driver.DB, error) {
 	gob.Register(models.User{})
 	gob.Register(models.Room{})
 	gob.Register(models.Restriction{})
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
 
 	// change this to true when in production
 	app.InProduction = false
